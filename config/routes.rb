f6f4e7c729855c61a3edb268a815a205410bbda8
Eoin Kelly <eoin@rabidtech.co.nz>
2019-06-03 15:44:33 +1200

    Add static page example

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
     end
   end
 
+  # It is probably good practice to register an anglicized version of the name
+  # too for those who can't easily type Te Reo on their keyboard
+  get "/maori", to: "pages#maori", as: :static_page_anglicized
+  get "/māori", to: "pages#maori", as: :static_page_original_maori
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "posts#index"
 end
